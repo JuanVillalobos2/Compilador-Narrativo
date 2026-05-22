edb4b21cb1b328bb1e0d4142b8cdc4dfaace3c01
JuanVillalobos2 <86274900+JuanVillalobos2@users.noreply.github.com>
2026-05-21 23:26:20 -0500

    Update Scaner.cpp

--- Scaner.cpp
+++ Scaner.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
-Lexer::Lexer(std::string texto) {
+Lexer::Lexer(string texto) {
     input = texto;
-    posicion = 0;
+    //posicion = 0;
 }
+
+bool esNumero(string palabra){
+        for(char c : palabra) {
+            if(!isdigit(c)) {
+                return false;
+            }
+        }   
+        return true;
+    }
 
 vector<Token> Lexer::analizar() {
 
@@ -18,24 +27,53 @@ vector<Token> Lexer::analizar() {
 
     while (ss >> palabra) {
 
-        if (palabra == "cumple") {
-            tokens.push_back(Token(TokenType::VERBO, palabra));
-        }
-
-        else if (palabra == "anio") {
-            tokens.push_back(Token(TokenType::UNIDAD, palabra));
+        if (palabra == "cumple" || palabra =="camina" || palabra =="sabe"|| palabra =="caminar") {
+            tokens.push_back(
+                Token(TokenType::VERBO, palabra)
+            );
         }
 
         else if (palabra == "un") {
-            tokens.push_back(Token(TokenType::NUMERO, "1"));
+            tokens.push_back(
+                Token(TokenType::NUMERO, "1")
+            );
+        }
+
+        else if (esNumero(palabra)) {
+            tokens.push_back(
+                Token(TokenType::NUMERO, palabra)
+            );
+        }
+
+        else if (palabra == "anio" || palabra == "anios") {
+            tokens.push_back(
+                Token(TokenType::UNIDAD, palabra)
+            );
+        }
+
+        else if (palabra == "y" || palabra == "ya" || palabra == "solo"|| palabra =="pero") {
+            tokens.push_back(
+                Token(TokenType::IRRELEVANTE, palabra)
+            );
+        }
+        
+        else if (palabra == "no"|| palabra =="nunca"|| palabra =="jamas") {
+            tokens.push_back(
+                Token(TokenType::ADVERBIO_NEGA, palabra)
+            );
         }
 
         else {
-            tokens.push_back(Token(TokenType::IDENTIFICADOR, palabra));
+
+            tokens.push_back(
+                Token(TokenType::IDENTIFICADOR, palabra)
+            );
         }
     }
 
-    tokens.push_back(Token(TokenType::FIN_ARCHIVO, "fin del archivo"));
+    tokens.push_back(
+        Token(TokenType::FIN_ARCHIVO, "fin del archivo")
+    );
 
     return tokens;
 }
